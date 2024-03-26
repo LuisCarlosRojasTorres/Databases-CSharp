47b75b6292aa2cd04956ebbd47c2900149820467
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-03-26 15:12:11 -0300

    Added to DB

--- DBs4Sharp/LiteDb4LiliApp/Program.cs
+++ DBs4Sharp/LiteDb4LiliApp/Program.cs
@@ -1,14 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Text.Json;
+using LiteDB;
 using LiteDb4LiliApp.Models;
 
 Dictionary<string, Dictionary<string, Medicamento>> medicamentos;
 
+static LiteDatabase CreateDB()
+{
+    LiteDatabase db = new LiteDatabase("LiliApp.db");
+    return db;
+}
+
+static void Add(ref LiteDatabase db, Dictionary<string, Dictionary<string, Medicamento>> m)
+{
+    foreach (var tipoDeMedicina in m)
+    {
+        foreach (var med in tipoDeMedicina.Value)
+        {
+            db.GetCollection<Medicamento>().Insert(med.Value);
+            db.Commit();
+        }
+    }
+}
+
+
+
 using (StreamReader file = File.OpenText(Path.Combine("Lili_demo.json")))
 {
-    medicamentos = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, Medicamento>>>(file.ReadToEnd());
+    medicamentos = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, Medicamento>>>(file.ReadToEnd());
 }
-Console.WriteLine("Done!");
+
+LiteDatabase db = CreateDB();
+Add(ref db, medicamentos);
+//Console.WriteLine("Done!");
 
 
 
