1209145693d3700afc6cb8c2f04787c43a9c751b
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-03-26 13:53:00 -0300

    Added System JSON for LiliApp

---
+++ DBs4Sharp/LiteDb4LiliApp/Models/Medicamento.cs
@@ -0,0 +1,24 @@
+﻿namespace LiteDb4LiliApp.Models
+{
+    public class Medicamento
+    {
+        public string NombreFarmacologico { get; set; }
+
+        public string NombreComercial { get; set; }
+
+
+        public string Grupo { get; set; }
+
+        public string PrincipioActivo { get; set; }
+
+        public string Accion { get; set; }
+
+        public List<string> Presentacion { get; set; }
+
+        public string Reconstitucion { get; set; }
+
+        public List<string> Cuidados { get; set; }
+
+        public List<string> Dosis { get; set; }
+    }
+}
--- DBs4Sharp/LiteDb4LiliApp/Program.cs
+++ DBs4Sharp/LiteDb4LiliApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+using LiteDb4LiliApp.Models;
+
+Dictionary<string, Dictionary<string, Medicamento>> medicamentos;
+
+using (StreamReader file = File.OpenText(Path.Combine("Lili_demo.json")))
+{
+    medicamentos = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, Medicamento>>>(file.ReadToEnd());
+}
+Console.WriteLine("Done!");
+
+
+
